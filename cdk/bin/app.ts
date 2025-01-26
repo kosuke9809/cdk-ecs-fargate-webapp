21b3f30aed1a6b216615288975d8f2b27d3eb9c7
kosuke9809 <uht0912k@outlook.com>
2025-01-26 22:42:37 +0900

    add: implement VPC and ECS app stack constructs with dynamic configuration loading

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -1,4 +1,40 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { IConfig } from '../config/interface';
+import * as path from 'path';
+import * as fs from 'fs';
+import { EcsAppStack } from '../lib/stack/ecs-app-stack';
 
 const app = new cdk.App();
+
+// settings
+
+type EnvContext = 'dev' | 'stg' | 'prd';
+
+function loadConfig(deployEnv: EnvContext): IConfig {
+  const configFile = path.join(__dirname, `../config/${deployEnv}.ts`);
+  if (!fs.existsSync(configFile)) {
+    throw new Error(`Can't find a ts environment file [../config/${deployEnv}.ts]`);
+  }
+  return require(configFile); // eslint-disable-line @typescript-eslint/no-require-imports
+}
+
+const argContext = 'environment';
+const envKey = app.node.tryGetContext(argContext);
+
+if (envKey == undefined) {
+  throw new Error(
+    `Please specify environment with context option. ex) cdk deploy -c ${argContext}=dev`,
+  );
+}
+
+const config: IConfig = loadConfig(envKey);
+
+// stasks
+new EcsAppStack(app, 'EcsApp', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  vpcProps: config.Vpc,
+});
---
+++ cdk/lib/construct/vpc-construct.ts
@@ -0,0 +1,33 @@
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+interface VpcConstructProps {
+  maxAzs: number;
+  natGateways: number;
+}
+
+export class VpcConstruct extends Construct {
+  public readonly myVpc: ec2.Vpc;
+  constructor(scope: Construct, id: string, props: VpcConstructProps) {
+    super(scope, id);
+
+    const myVpc = new ec2.Vpc(this, 'MyVpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      maxAzs: props.maxAzs,
+      natGateways: props.natGateways,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'Public',
+        },
+        {
+          cidrMask: 24,
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          name: 'Private',
+        },
+      ],
+    });
+    this.myVpc = myVpc;
+  }
+}
