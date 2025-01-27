aa56e91d0fe7051a0a3a6b1b128f832fa9ebfe4c
kosuke9809 <uht0912k@outlook.com>
2025-01-27 23:24:32 +0900

    wip

---
+++ cdk/lib/construct/ecs-app-construct.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+
+export interface EcsAppConstructProps {
+  alb?: elbv2.ApplicationLoadBalancer;
+}
