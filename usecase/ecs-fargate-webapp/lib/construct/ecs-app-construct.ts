d0f8045a0ac4365bef17db451ff8c3e30880811a
kosuke9809 <uht0912k@outlook.com>
2025-01-28 23:17:11 +0900

    add: ecs app construct

--- usecase/ecs-fargate-webapp/lib/construct/ecs-app-construct.ts
+++ usecase/ecs-fargate-webapp/lib/construct/ecs-app-construct.ts
@@ -1,10 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export interface EcsAppConstructProps {
-  alb?: elbv2.ApplicationLoadBalancer;
+  serviceName: string;
+  removalPolicy: cdk.RemovalPolicy;
+  autoDeleteImages: boolean;
+}
+
+export class EcsAppConstruct extends Construct {
+  public readonly taskRole: iam.Role;
+  constructor(scope: Construct, id: string, props: EcsAppConstructProps) {
+    super(scope, id);
+
+    const ecrRepository = new ecr.Repository(this, 'EcrRepository', {
+      repositoryName: `${props.serviceName}-repository`.slice(0, 256),
+      removalPolicy: props.removalPolicy,
+      autoDeleteImages: props.autoDeleteImages,
+    });
+
+    const taskRole = new iam.Role(this, 'TaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+
+    const taskExecRole = new iam.Role(this, 'TaskExecRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'),
+      ],
+    });
+
+    new cdk.CfnOutput(this, 'EcrRepositoryArn', {
+      value: ecrRepository.repositoryArn,
+      description: `${props.serviceName} ECR Repository ARN`,
+    });
+
+    new cdk.CfnOutput(this, 'TaskRoleArn', {
+      value: taskRole.roleArn,
+      description: `${props.serviceName} Task Role ARN`,
+    });
+
+    new cdk.CfnOutput(this, 'TaskExecRoleArn', {
+      value: taskExecRole.roleArn,
+      description: `${props.serviceName} Task Execution Role ARN`,
+    });
+
+    this.taskRole = taskRole;
+  }
 }
