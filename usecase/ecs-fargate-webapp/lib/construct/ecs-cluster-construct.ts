ac76310146253bfd6137193eaed36289c4c59466
kosuke9809 <uht0912k@outlook.com>
2025-02-04 00:04:32 +0900

    feat: add SSM parameters for ECS cluster name and namespace in EcsClusterConstruct

--- usecase/ecs-fargate-webapp/lib/construct/ecs-cluster-construct.ts
+++ usecase/ecs-fargate-webapp/lib/construct/ecs-cluster-construct.ts
@@ -1,9 +1,12 @@
 import { Construct } from 'constructs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 interface EcsClusterConstructProps {
+  servicePrefix: string;
   vpc: ec2.Vpc;
+  namespaceName: string;
 }
 
 export class EcsClusterConstruct extends Construct {
@@ -13,6 +16,20 @@ export class EcsClusterConstruct extends Construct {
 
     const cluster = new ecs.Cluster(this, 'EcsCluster', {
       vpc: props.vpc,
+    });
+
+    const namespace = cluster.addDefaultCloudMapNamespace({
+      name: props.namespaceName,
+    });
+
+    new ssm.StringParameter(this, 'EcsClusterName', {
+      parameterName: `/${props.servicePrefix}/ecs-cluster-name`,
+      stringValue: cluster.clusterName,
+    });
+
+    new ssm.StringParameter(this, 'EcsNameSpace', {
+      parameterName: `/${props.servicePrefix}/ecs-namespace`,
+      stringValue: namespace.namespaceName,
     });
 
     this.cluster = cluster;
