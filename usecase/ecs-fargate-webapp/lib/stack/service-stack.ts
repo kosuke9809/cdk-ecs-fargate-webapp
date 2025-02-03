463db16a9bb08224d53cdfd8be80d496c2965dda
kosuke9809 <uht0912k@outlook.com>
2025-02-04 00:04:38 +0900

    feat: refactor service name handling to use service prefix and add security group for ECS app

--- usecase/ecs-fargate-webapp/lib/stack/service-stack.ts
+++ usecase/ecs-fargate-webapp/lib/stack/service-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { VpcConstruct } from '../construct/vpc-construct';
 import { EcsClusterConstruct } from '../construct/ecs-cluster-construct';
+import { AlbConstruct } from '../construct/alb-construct';
+import { EcsAppConstruct } from '../construct/ecs-app-construct';
 
 export interface VpcProps {
   maxAzs: number;
@@ -24,9 +26,29 @@ export class ServiceStack extends cdk.Stack {
     });
 
     const ecsCluster = new EcsClusterConstruct(this, 'EcsAppCluster', {
+      servicePrefix: props.servicePrefix,
       vpc: myVpc.myVpc,
+      namespaceName: 'ecs.local',
     });
 
     // system resources
+    const alb = new AlbConstruct(this, 'Alb', {
+      vpc: myVpc.myVpc,
+      servicePrefix: props.servicePrefix,
+    });
+
+    const frontend = new EcsAppConstruct(this, 'Frontend', {
+      serviceName: `${props.servicePrefix}-frontend`,
+      vpc: myVpc.myVpc,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteImages: true,
+    });
+
+    const backend = new EcsAppConstruct(this, 'Backend', {
+      serviceName: `${props.servicePrefix}-backend`,
+      vpc: myVpc.myVpc,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteImages: true,
+    });
   }
 }
